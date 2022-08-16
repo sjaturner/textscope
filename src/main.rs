0289b7f7d34985e1632397361edde3f018bff3fb
Simon Turner <sjaturner@googlemail.com>
2022-08-16 18:28:43 +0100

    added the command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use std::io::{stdout, Write};
+use std::io::stdout;
+use crossterm::{cursor, style::Print, ExecutableCommand, Result};
+use clap::Parser;
 
-use crossterm::{
-    event, 
-    execute,
-    cursor,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand, 
-    Result,
-};
+/// This is a simple text scope which reads stdin and expect two input values per line
+/// The first input value is epoch
+/// The second is the reading to be displayed on the scope
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   /// Number of columns to use
+   #[clap(short, long, value_parser, default_value_t = 80)]
+   columns: u8,
+
+   /// The maximum value expected in the data column
+   #[clap(short, long, value_parser, default_value_t = 256.0)]
+   max_vals: f32,
+
+   /// Number of rows to use
+   #[clap(short, long, value_parser, default_value_t = 25)]
+   rows: u8,
+
+   /// Time step per column
+   #[clap(short, long, value_parser, default_value_t = 1.0)]
+   step: f32,
+}
 
 fn main() -> Result<()> {
-    for _ in 1..=10 {
-        stdout()
-            .execute(Print("\n"))?;
+   let args = Args::parse();
+
+    for _ in 1..=args.rows {
+        stdout().execute(Print("\n"))?;
     }
     stdout()
         .execute(cursor::SavePosition)?
