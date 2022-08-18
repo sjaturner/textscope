23006c90eb58ab98bd124418c34de1cce5817102
Simon Turner <sjaturner@googlemail.com>
2022-08-18 23:26:15 +0100

    put a box around this

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ use signal_hook::flag;
 use std::collections::VecDeque;
 use std::io;
 use std::io::stdout;
-use std::io::Error;
+use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::TryRecvError;
-use std::sync::Arc;
 use std::thread;
-use std::time;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// This is a simple text scope which reads stdin and expect two input values per line
@@ -45,11 +43,6 @@ fn now() -> f64 {
     since_the_epoch.as_millis() as f64 / 1000.0
 }
 
-fn sleep(millis: u64) {
-    let duration = time::Duration::from_millis(millis);
-    thread::sleep(duration);
-}
-
 fn spawn_stdin_channel() -> Receiver<String> {
     let (tx, rx) = mpsc::channel::<String>();
     thread::spawn(move || loop {
@@ -78,6 +71,10 @@ fn main() -> Result<()> {
         match stdin_channel.try_recv() {
             Ok(line) => {
                 let input_values: Vec<_> = line.trim().split_whitespace().collect();
+
+                if input_values.len() != 2 {
+                    break;
+                }
                 let epoch: f64 = input_values[0].parse().unwrap();
                 let value: f64 = input_values[1].parse().unwrap();
                 deque.push_front((epoch, value));
@@ -103,16 +100,33 @@ fn main() -> Result<()> {
             }
         }
 
-        let mut display = vec![vec!['#'; args.columns as usize]; args.rows as usize];
+        let mut display = vec![vec![' '; args.columns as usize]; args.rows as usize];
+
+        for elem in display[0].iter_mut() {
+            *elem = '-';
+        }
+
+        let last_row_index = (args.rows - 1) as usize;
+        for elem in display[last_row_index].iter_mut() {
+            *elem = '-';
+        }
+
+        let last_column_index = (args.columns - 1) as usize;
+        for row in 0..args.rows {
+            display[row as usize][last_column_index] = '|';
+        }
+
+        display[0][last_column_index] = '+';
+        display[last_row_index][last_column_index] = '+';
 
         for elem in &deque {
             let (epoch, value) = elem;
             let seconds_since_base = epoch - base_epoch;
             let column = (seconds_since_base / args.step).clamp(0.0, args.columns as f64 - 1.0);
             let value = value.clamp(0.0, args.max_vals);
-            let row = (value / args.max_vals * args.rows as f64);
+            let row = value / args.max_vals * args.rows as f64;
 
-            display[row as usize][column as usize] = 'O';
+            display[row as usize][column as usize] = '+';
         }
 
         if term.load(Ordering::Relaxed) {
@@ -125,8 +139,10 @@ fn main() -> Result<()> {
 
         for line in display {
             let s: String = line.into_iter().collect();
-            stdout().execute(Print(s))?;
-            stdout().execute(Print("\n"))?;
+            stdout()
+                .execute(Print(s))?
+                .execute(cursor::MoveDown(1))?
+                .execute(cursor::MoveToColumn(0))?;
         }
     }
 
